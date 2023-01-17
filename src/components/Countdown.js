8948deb226c96fd7c269f3ed7f73a883472e5aa6
skandog <muumrar@gmail.com>
2023-01-17 17:20:29 +0000

    added minute and second const which takes the millis and uses floor and % to display current time, need to formattime

--- src/components/Countdown.js
+++ src/components/Countdown.js
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { Text, View, StyleSheet } from "react-native";
+
 import { colors } from "../utils/colors";
 import { fontSizes, spacingSizes } from "../utils/sizes";
 
 const minutesToMillis = (min) => min * 1000 * 60;
 
 export const Countdown = ({ minutes = 20, isPaused }) => {
+  const [millis, setMillis] = useState(minutesToMillis(minutes));
+
+  const minute = Math.floor(millis / 1000 / 60) % 60;
+  const second = Math.floor(millis / 1000) % 60;
   return (
     <View>
-      <Text style={styles.text}>Timer here</Text>
+      <Text style={styles.text}>
+        {minute}:{second}
+      </Text>
     </View>
   );
 };
