11ace6bf61b0f91e394c96354f3f3a2ed248f74b
skandog <muumrar@gmail.com>
2023-01-17 14:24:41 +0000

    setup focus feature

---
+++ src/features/focus/Focus.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export const Focus = () => {
+  return (
+    <View>
+      <Text>What you working on?</Text>
+    </View>
+  );
+};
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#454545'
+    }
+})
